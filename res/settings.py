3744b14d90a351343dd2201d86e16d7e24d987ab
hungnt2 <hung.nguyen.the@beetechjsc.com>
2024-06-06 18:19:59 +0700

    fix error vendor

--- res/settings.py
+++ res/settings.py
@@ -145,21 +145,21 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
 # Add s3 in aws
-AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME', default=False)
-if AWS_STORAGE_BUCKET_NAME:
-    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-    AWS_DEFAULT_ACL = 'public-read'
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-
-    # s3 public media settings
-    PUBLIC_MEDIA_LOCATION = 'media'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-else:
-    MEDIA_URL = '/media/'
-    MEDIA_ROOT = BASE_DIR / "media"
+# AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME', default=False)
+# if AWS_STORAGE_BUCKET_NAME:
+#     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+#     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+#     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+#     AWS_DEFAULT_ACL = 'public-read'
+#     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+#     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+#
+#     # s3 public media settings
+#     PUBLIC_MEDIA_LOCATION = 'media'
+#     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+# else:
+#     MEDIA_URL = '/media/'
+#     MEDIA_ROOT = BASE_DIR / "media"
 CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", 'redis://localhost:6379')
 CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", 'redis://localhost:6379')
 CELERY_ACCEPT_CONTENT = ['application/json']
--- vendor/views.py
+++ vendor/views.py
@@ -2,25 +2,37 @@ from django.shortcuts import render
 
 from employee.forms import RegisterForm
 from vendor.forms import VendorForm
+from django.conf import settings
+import os
 
 
+def upload_file(file_name):
+    if file_name:
+        file_path = os.path.join(settings.MEDIA_ROOT, file_name.name)
+
+        with open(file_path, 'wb+') as destination:
+            for chunk in file_name.chunks():
+                destination.write(chunk)
+        file_url = settings.MEDIA_URL + file_name.name
+        return file_url
+    return None
 # Create your views here.
 def register_vendor(request):
     print("request.FILES['filename'].name", request.FILES)
     print('register_user333333', request.POST, request.FILES)
-    vendor_license_filename = request.POST.get('vendor_license', '')
 
     form = RegisterForm(request.POST or None)
     vendor_form = VendorForm(request.POST or None, request.FILES or None)
     ctx = {
         "form": form,
         "vendor_form": vendor_form,
-        "vendor_license_filename": vendor_license_filename,
     }
     print('register_user333333', ctx)
     if request.method == 'POST':
         if request.POST.get('next', '') == 'confirm':
             if form.is_valid() and vendor_form.is_valid():
+                upload_file_url = upload_file(request.FILES['vendor_license'])
+                ctx['upload_file_url'] = upload_file_url
                 return render(request, 'vendor/register_vendor_cf.html', ctx)
         if request.POST.get('next', '') == 'back':
             return render(request, 'vendor/register_vendor.html', ctx)
@@ -38,3 +50,6 @@ def register_vendor(request):
                 return render(request, 'vendor/register_save.html', ctx)
 
     return render(request, 'vendor/register_vendor.html', ctx)
+
+
+
