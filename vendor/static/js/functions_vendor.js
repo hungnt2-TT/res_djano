0783f75bc404cf34a2448d8ca429b554ebc0e7a2
hungnt2 <hung.nguyen.the@beetechjsc.com>
2024-06-10 15:16:56 +0700

    update

--- vendor/static/js/functions_vendor.js
+++ vendor/static/js/functions_vendor.js
@@ -1,16 +1,26 @@
-$(document).ready(function() {
+document.getElementById('imageInput').addEventListener('change', function(event) {
+            const file = event.target.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = function(e) {
+                    document.getElementById('previewImage').src = e.target.result;
+                }
+                reader.readAsDataURL(file);
+            }
+        });
+$(document).ready(function () {
     $('#imageGallery').lightSlider({
         gallery: true,
-        item:1,
-        loop:true,
-        thumbItem:9,
-        slideMargin:0,
+        item: 1,
+        loop: true,
+        thumbItem: 9,
+        slideMargin: 0,
         enableDrag: false,
-        currentPagerPosition:'left',
-        onSliderLoad: function(el) {
+        currentPagerPosition: 'left',
+        onSliderLoad: function (el) {
             el.lightGallery({
                 selector: '#imageGallery .lslide'
             });
         }
     })
-}
+})
