20e00e5cb91a66bebc95cf2fc10719af231f747c
hungnt2 <hung.nguyen.the@beetechjsc.com>
2024-06-14 18:01:38 +0700

    fix vendor send and add screen login

--- wallet/models.py
+++ wallet/models.py
@@ -1,19 +1,23 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.conf import settings
 
 from employee.models import Profile
 import uuid
 from django.urls import reverse
 
+CURRENCY_STORE_FIELD = getattr(settings,
+                               'WALLET_CURRENCY_STORE_FIELD', models.BigIntegerField)
+
 
 class PaymentMethod(models.Model):
     PAYMENT_METHOD_UNKNOWN = 0
     PAYMENT_METHOD_BANK_TRANSFER = 1
-    PAYMENT_METHOD_PAYPAL = 2
+    PAYMENT_METHOD_PAY = 2
     PAYMENT_METHOD_CHOICES = (
         (PAYMENT_METHOD_UNKNOWN, 'unknown'),
         (PAYMENT_METHOD_BANK_TRANSFER, 'bank transfer'),
-        (PAYMENT_METHOD_PAYPAL, 'paypal')
+        (PAYMENT_METHOD_PAY, 'pay')
     )
     user = models.OneToOneField(Profile, on_delete=models.CASCADE)
     payment_method = models.IntegerField(choices=PAYMENT_METHOD_CHOICES, default=PAYMENT_METHOD_UNKNOWN)
@@ -21,6 +25,8 @@ class PaymentMethod(models.Model):
     account_number = models.CharField(_("account number"), max_length=100)
     bank = models.CharField(_("bank"), max_length=100)
     paypal_email = models.EmailField(_("paypal email"), max_length=255, blank=True, null=True)
+    expiry_date = models.DateField(_("expiry date"), blank=True, null=True)
+    cvv = models.CharField(_("cvv"), max_length=3, blank=True, null=True)
     create_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -37,11 +43,8 @@ class PaymentMethod(models.Model):
 class Wallet(models.Model):
     uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.OneToOneField(Profile, on_delete=models.CASCADE)
-    balance = models.DecimalField(_("balance"), max_digits=100, decimal_places=2)
-    account_name = models.CharField(_("account name"), max_length=250)
-    account_number = models.CharField(_("account number"), max_length=100)
-    bank = models.CharField(_("bank"), max_length=100)
-    # payment = models.OneToOneField(PaymentMethod, on_delete=models.CASCADE, blank=True, null=True)
+    balance = CURRENCY_STORE_FIELD(default=0)
+    payment_method = models.OneToOneField(PaymentMethod, on_delete=models.CASCADE, blank=True, null=True)
     create_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -61,18 +64,42 @@ class Wallet(models.Model):
         self.balance = round(self.balance, 2)
         super(Wallet, self).save(*args, **kwargs)
 
+    def deposit(self, value):
+        self.transaction_set.create(
+            transaction_type=Transaction.TRANSACTION_TYPE_DEPOSIT,
+            value=value,
+            running_balance=self.balance + value,
+            description='Deposit'
+        )
+        self.balance += value
+        self.save()
+
+    def withdraw(self, value):
+        self.transaction_set.create(
+            transaction_type=Transaction.TRANSACTION_TYPE_WITHDRAW,
+            value=value,
+            running_balance=self.balance - value,
+            description='Withdraw'
+        )
+        self.balance -= value
+        self.save()
+
+    def transfer(self, wallet, value):
+        self.withdraw(value)
+        wallet.deposit(value)
+
+
 class Transaction(models.Model):
-    TRANSACTION_TYPE_UNKNOWN = 0
     TRANSACTION_TYPE_DEPOSIT = 1
     TRANSACTION_TYPE_WITHDRAW = 2
     TRANSACTION_TYPE_CHOICES = (
-        (TRANSACTION_TYPE_UNKNOWN, 'unknown'),
         (TRANSACTION_TYPE_DEPOSIT, 'deposit'),
         (TRANSACTION_TYPE_WITHDRAW, 'withdraw')
     )
     wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
-    transaction_type = models.IntegerField(choices=TRANSACTION_TYPE_CHOICES, default=TRANSACTION_TYPE_UNKNOWN)
-    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    transaction_type = models.IntegerField(choices=TRANSACTION_TYPE_CHOICES)
+    value = CURRENCY_STORE_FIELD(default=0)
+    running_balance = CURRENCY_STORE_FIELD(default=0)
     receiver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='receiver', blank=True, null=True)
     creator = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='creator', blank=True, null=True)
     payment_method = models.ForeignKey(PaymentMethod, on_delete=models.CASCADE, blank=True, null=True)
@@ -95,16 +122,14 @@ class Transaction(models.Model):
 
 
 class SubTransaction(models.Model):
-    TRANSACTION_TYPE_UNKNOWN = 0
     TRANSACTION_TYPE_DEPOSIT = 1
     TRANSACTION_TYPE_WITHDRAW = 2
     TRANSACTION_TYPE_CHOICES = (
-        (TRANSACTION_TYPE_UNKNOWN, 'unknown'),
         (TRANSACTION_TYPE_DEPOSIT, 'deposit'),
         (TRANSACTION_TYPE_WITHDRAW, 'withdraw')
     )
     transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
-    transaction_type = models.IntegerField(choices=TRANSACTION_TYPE_CHOICES, default=TRANSACTION_TYPE_UNKNOWN)
+    transaction_type = models.IntegerField(choices=TRANSACTION_TYPE_CHOICES)
     amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     description = models.TextField()
     create_at = models.DateTimeField(auto_now_add=True)
@@ -117,30 +142,3 @@ class SubTransaction(models.Model):
         db_table = 'sub_transaction'
         verbose_name = 'Sub Transaction'
         verbose_name_plural = 'Sub Transactions'
-
-
-class PaymentMethod(models.Model):
-    PAYMENT_METHOD_UNKNOWN = 0
-    PAYMENT_METHOD_BANK_TRANSFER = 1
-    PAYMENT_METHOD_PAYPAL = 2
-    PAYMENT_METHOD_CHOICES = (
-        (PAYMENT_METHOD_UNKNOWN, 'unknown'),
-        (PAYMENT_METHOD_BANK_TRANSFER, 'bank transfer'),
-        (PAYMENT_METHOD_PAYPAL, 'paypal')
-    )
-    user = models.OneToOneField(Profile, on_delete=models.CASCADE)
-    payment_method = models.IntegerField(choices=PAYMENT_METHOD_CHOICES, default=PAYMENT_METHOD_UNKNOWN)
-    account_name = models.CharField(_("account name"), max_length=250)
-    account_number = models.CharField(_("account number"), max_length=100)
-    bank = models.CharField(_("bank"), max_length=100)
-    paypal_email = models.EmailField(_("paypal email"), max_length=255, blank=True, null=True)
-    create_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.user.username
-
-    class Meta:
-        db_table = 'payment_method'
-        verbose_name = 'Payment Method'
-        verbose_name_plural = 'Payment Methods'
